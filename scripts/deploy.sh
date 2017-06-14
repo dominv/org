f1ed478e85267cb7855e30735d5539d686644956
Domingo Valera <domingo.v@47deg.com>
2017-06-14 16:17:52 +0200

    Updating deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,17 +3,17 @@
 # Configure git
 
 git config --global user.name "Travis CI"
-git config --global user.email "ana@47deg.com"
+git config --global user.email "domingo.v@47deg.com"
 
 # Pull latest version of published subtree
 git checkout master
-git subtree pull --prefix=docs --message="[skip ci] Update subtree" https://47deg:$GITHUB_API_KEY@github.com/47deg/47deg.github.io.git master
+git subtree pull --prefix=docs --message="[skip ci] Update subtree" https://dominv:$GITHUB_API_KEY@github.com/dominv/dominv.github.io.git master
 
 # Build & Commit built site
 lein run
 git add docs
 git commit -m "[skip ci] Generate site"
-git push https://47deg:$GITHUB_API_KEY@github.com/47deg/org master
+git push https://dominv:$GITHUB_API_KEY@github.com/dominv/org master
 
 # Push built subtree to official website
-git subtree push --prefix=docs https://47deg:$GITHUB_API_KEY@github.com/47deg/47deg.github.io.git master
+git subtree push --prefix=docs https://dominv:$GITHUB_API_KEY@github.com/dominv/dominv.github.io.git master
